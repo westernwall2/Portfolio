37e170209ad48705ffbb30a422449f89d0fcb487
Jeferson Costa <86005020+westernwall2@users.noreply.github.com>
2023-01-15 11:36:09 -0300

    portfolio 001
    Foi inserido os arquivos de imagem e inicio da construção da pagina

--- js/chamada.js
+++ js/chamada.js
@@ -1,1 +1,14 @@
-console.log("Olá Mundo!")
+var nav = document.getElementById('nav')
+
+if (!!nav == true){
+    nav.innerHTML = 
+    `
+        <img class="icone" src="/img/logo minimalista.png" alt="">
+        <strong>Portfólio</strong>
+        <ul class="recipiente menu">
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Projeto</a></li>
+            <li><a href="">Contato</a></li>
+        </ul>
+    `
+}
