b0b0f5b5022f7594bbe1509fe925d0308299dcdf
Jeferson Costa <86005020+westernwall2@users.noreply.github.com>
2024-06-27 19:54:34 -0300

    Portifolio 008

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 /*Seleçao de linguagem*/
 const translations = {
     en: {
-        about: "About Us",
-        services: "Services",
+        home: "Home",
         projects: "Projects",
         contact: "Contact",
-        aboutUsContent: "With a solid background in Node.js, HTML, CSS, JavaScript, SQL Server and MySQL, I am committed to delivering projects that not only meet but exceed client expectations.",
-        contactContent: "Contact us via email jefersoncosta1994@gmail.com or by phone (71) 99700-2418. You can also find us on Instagram: @aliancadev."
+        salute: 'Hello, <span class="destaque">I am</span>',
+        about: "Full Stack Web Developer <br> with experience in creating dynamic and interactive solutions.",
+        aboutUsContent: 'With a solid background in <span class="tags">Node.js</span>, <span class="tags">HTML</span>, <span class="tags">CSS</span>, <span class="tags">JavaScript</span>, <span class="tags">SQL Server</span> and <span class="tags">MySQL</span>, I am committed to delivering projects that not only meet but exceed client expectations.',
+        ContactUs: "Contact Us"
     },
     pt: {
-        about: "Sobre Nós",
-        services: "Serviços",
+        home: "Início",
         projects: "Projetos",
         contact: "Contato",
-        aboutUsContent: "Com um sólido background em Node.js, HTML, CSS, JavaScript, SQL Server e MySQL, estou comprometido em entregar projetos que não apenas atendam, mas superem as expectativas dos clientes.",
-        servicesContent: "Criação de sistemas para pequenos negócios, banco de dados, sites, automação residencial.",
-        contactContent: "Entre em contato conosco através do e-mail jefersoncosta1994@gmail.com ou pelo telefone (71) 99700-2418. Você também pode nos encontrar no Instagram: @aliancadev."
+        salute: 'Olá, <span class="destaque">Eu sou</span>',
+        about: "Desenvolvedor Web Full Stack <br> com experiência em criar soluções dinâmicas e interativas.",
+        aboutUsContent: 'Com um sólido background em <span class="tags">Node.js</span>, <span class="tags">HTML</span>, <span class="tags">CSS</span>, <span class="tags">JavaScript</span>, <span class="tags">SQL Server</span> e <span class="tags">MySQL</span>, estou comprometido em entregar projetos que não apenas atendam, mas superem as expectativas dos clientes.',
+        ContactUs: "Entre em contato"
     }
 };
 
@@ -23,17 +24,40 @@ const languageSelect = document.getElementById('language-select');
 languageSelect.addEventListener('change', function() {
     let language = languageSelect.value
     
-    //document.getElementById('about-title').textContent = translations[language].about;
+    document.getElementById('home').textContent = translations[language].home;
+    document.getElementById('projects').textContent = translations[language].projects;
+    document.getElementById('contact').textContent = translations[language].contact;
+    document.getElementById('salute').innerHTML = translations[language].salute;
+    document.getElementById('about').innerHTML = translations[language].about;
     //document.getElementById('services-title').textContent = translations[language].services;
     //document.getElementById('projects-title').textContent = translations[language].projects;
     //document.getElementById('contact-title').textContent = translations[language].contact;
-    document.getElementById('about-content').textContent = translations[language].aboutUsContent;
-    //document.getElementById('services-content').textContent = translations[language].servicesContent;
+    document.getElementById('about-content').innerHTML = translations[language].aboutUsContent;
+    document.getElementById('whatsapp').textContent = translations[language].ContactUs;
     //document.getElementById('contact-content').textContent = translations[language].contactContent;
-
+    //document.getElementById('contact-content').textContent = translations[language].contactContent;
+    tagColor()
 });
 
+const colors = {
+    'Node.js': '#417e38', 
+    'HTML': '#e76027', 
+    'CSS': '#136eb4', 
+    'JavaScript': '#f1a93b', 
+    'SQL Server': '#b11d1d', 
+    'MySQL': '#035c7f'
+}
 
+function tagColor() {
+    let tags = document.querySelectorAll('.tags');
+
+    for(i= 0; i < tags.length; i++){
+        let color = colors[tags[i].textContent]
+        tags[i].style.background = color
+    }
+}
+
+tagColor()
 /*Botao de WhatsAp*/
 const whatsapp = document.querySelector('#whatsapp')
 whatsapp.addEventListener('click', ()=>{
